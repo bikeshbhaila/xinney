3cf4d4a627b8dc2fa7e72eb29eecaf0b203688b5
Bikesh Bhaila <bhailabikesh1@gmail.com>
2020-09-11 19:22:06 +0545

    completed design part

--- js/script.js
+++ js/script.js
@@ -1,4 +1,16 @@
 $(function(){
-    $("#header").load("header.html");
     $("#footer").load("footer.html");
 });
+
+
+$(document).ready(function(){
+    $('.navbar-toggler, .nav-button').on("click", function(){
+      $(".mobile-menu, .overlay").toggleClass('open');
+    });
+
+    $('.nav-button').click(function(){
+      $('.nav-button').toggleClass('change');
+    });
+
+  });
+
